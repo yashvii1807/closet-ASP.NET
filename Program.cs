7256d2fd541ffa606bdd0d7a75dd0af8abaf25a4
Shah Prem <112742383+Premshah431@users.noreply.github.com>
2026-04-12 10:04:20 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,12 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
 
 var app = builder.Build();
 
@@ -25,7 +19,6 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
